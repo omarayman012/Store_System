536d2a1f8bb93e22f6f4c00d11185bbf693b22fe
abdallahayman00 <bodaayman7399@gmail.com>
2025-04-23 19:04:41 +0200

    edits

---
+++ Store_Sys/Controllers/StoreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store_Sys.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
