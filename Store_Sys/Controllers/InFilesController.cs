8fc12621cdb39340e5e914ae54d55361f193136a
abdallahayman00 <bodaayman7399@gmail.com>
2025-04-24 19:05:07 +0200

    edits

---
+++ Store_Sys/Controllers/InFilesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store_Sys.Controllers
+{
+    public class InFilesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddFile()
+        {
+            return View("AddFile");
+        }
+        public IActionResult EditFile()
+        {
+            return View("EditFile");
+
+        }
+    }
+}
---
+++ Store_Sys/Controllers/OutFilesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store_Sys.Controllers
+{
+    public class OutFilesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddFile()
+        {
+            return View("AddFile");
+        }
+        public IActionResult EditFile()
+        {
+            return View("EditFile");
+
+        }
+    }
+}
